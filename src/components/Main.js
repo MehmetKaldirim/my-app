9f63f9d69ce78767c3d5dd418eff83e70ed02322
Mehmet <mathdenizi@gmail.com>
2022-06-20 18:32:05 +0200

    first static page with React is ready

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,17 @@ import React from "react";
 
 export function Main() {
   return (
-    <div className="App">
-      <Page />
-    </div>
+    <main>
+      <h1 className="main--title">Fun facts about React</h1>
+      <ul className="main--facts">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile</li>
+      </ul>
+      <Footer />
+    </main>
   );
 }
 
@@ -13,28 +21,5 @@ function Footer() {
     <footer>
       <small>© 2022 Math development. All rights reserved.</small>
     </footer>
-  );
-}
-
-function MainContent() {
-  return (
-    <div>
-      <h1>Reasons I'm excited to learn React</h1>
-      <ol>
-        <li>
-          It's a popular library, so I'll be able to fit in with the cool kids!
-        </li>
-        <li>I'm more likely to get a job as a developer if I know React</li>
-      </ol>
-    </div>
-  );
-}
-
-function Page() {
-  return (
-    <div>
-      <MainContent />
-      <Footer />
-    </div>
   );
 }
